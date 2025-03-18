b25822e19f82848a673223787740e2c2dc1b44a0
Adam Sarhan <66484976+adamsa09@users.noreply.github.com>
2025-03-17 23:36:38 -0400

    Add download playlist functionnality

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ banner = """
 ------------------------------------------------------------------------
 
 
-
 """
 
 print(banner)
@@ -64,32 +63,95 @@ def compile_search_url(name, artist):
     search_url = urllib.parse.quote(full_url, safe='/:?=&', encoding=None, errors=None)
     return search_url
 
-def create_mp3(song):
+def create_mp3(song_name):
     for file in os.listdir():
         if file.endswith('.mp4'):
             videofile = file
-
-    video = VideoFileClip(videofile)
-    video.audio.write_audiofile(f'{song}.mp3', codec='libmp3lame')
+            video = VideoFileClip(videofile)
+            if song_name != '':
+                video.audio.write_audiofile(f'{song}.mp3', codec='libmp3lame')
+            else:
+                # If no song name is supplied, use the video name
+                video.audio.write_audiofile(f'{file.removesuffix('.mp4')}.mp3', codec='libmp3lame')
+            video.close()
+            os.remove(videofile)
 
 def cleanup():
-    for file in os.listdir():
-        if file.endswith('.mp4'):
-            os.remove(file)
+    pass
+    # to_remove = []
+    # for file in os.listdir():
+    #     if file.endswith('.mp4'):
+    #         to_remove.append(file)
 
+    # # Close any open video handles
+    # import gc
+    # gc.collect()
+
+    # for file in to_remove:
+    #     try:
+    #         os.remove(file)
+    #     except PermissionError:
+    #         print(f"[!] Could not remove {file}. File may be in use.")
+    
 def apply_tags(song, artist):
     audiofile = eyed3.load(f'{song}.mp3')
     audiofile.tag.title = song
     audiofile.tag.artist = artist
     audiofile.tag.save()
 
-def send_to_spotify_folder(song):
+def send_to_spotify_folder():
     config = json.loads(open('config.json', 'r').read())
     if config['spotify-save-location'] == '':
         print('Please set the Spotify folder path in the config file')
         return
     
-    os.rename(f'{song}.mp3', f'{config["spotify-save-location"]}/{song}.mp3')
+    for file in os.listdir():
+        if file.endswith('.mp3'):
+            # Send the file to the spotify save directory
+            os.rename(file, f'{config["spotify-save-location"]}/{file}')
+
+def getSong(song, artist):
+    print('[i] Retrieving Song link')
+
+    search_url = compile_search_url(song, artist)
+
+    song_link = get_song_links(search_url)[0]
+
+    print(f'[i] Found Song: {song_link}')
+
+    YouTube(song_link).streams.first().download()
+
+    print('[i] Extracting .mp3 from song')
+
+    create_mp3(song)
+
+    print('[i] Applying Metadata')
+
+    apply_tags(song, artist)
+
+    print('[i] Moving to Spotify')
+
+    send_to_spotify_folder()
+
+    print('[i] Cleaning up')
+    cleanup()
+
+def getPlaylist(playlist_link):
+    print('[i] Retrieving Playlist links')
+    
+    all_links = get_song_links(playlist_link)
+    song_links = []
+    for k in range(len(all_links)):
+        if 'index' in all_links[k] and all_links[k] not in song_links:
+            song_links.append(all_links[k])
+    
+    for i in range(len(song_links)):
+        YouTube(song_links[i]).streams.first().download()
+        create_mp3('')
+        # TODO: Apply tags for each mp3 created.
+        send_to_spotify_folder()
+        cleanup()
+
 
 # -------------------------------------------------------------------
 
@@ -100,28 +162,7 @@ mode = input('Select mode: ')
 if int(mode) == 1:
     song = input('Song Name: ')
     artist = input('Song Artist: ')
-
-print('[i] Retrieving Song link')
-
-search_url = compile_search_url(song, artist)
-
-song_link = get_song_links(search_url)[0]
-
-print(f'[i] Found Song: {song_link}')
-
-YouTube(song_link).streams.first().download()
-
-print('[i] Extracting .mp3 from song')
-
-create_mp3(song)
-
-print('[i] Applying Metadata')
-
-apply_tags(song, artist)
-
-print('[i] Moving to Spotify')
-
-send_to_spotify_folder(song)
-
-print('[i] Cleaning up')
-cleanup()
+    getSong(song, artist)
+elif int(mode) == 2:
+    playlist_link = input('Playlist Link: ')
+    getPlaylist(playlist_link)
